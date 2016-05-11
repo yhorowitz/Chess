7279f1e53bcb1de043dca01c1f36b768b3246569
Levi Perelmuter <shortylbc@gmail.com>
2016-05-10 23:38:45 -0400

    Knight Moves

--- src/chess/model/pieces/Knight.java
+++ src/chess/model/pieces/Knight.java
@@ -21,7 +21,51 @@ public class Knight extends ChessPiece  {
 
     @Override
     public List<Position> getLegalMoves(ChessGame game, boolean removeMovesThatCauseCheck) {
-        return new ArrayList<>();
+        List<Position> legalMoves = new ArrayList<>();
+    	
+    	Position p1 = new Position(0,0);Position p2 = new Position(0,0);Position p3 = new Position(0,0);
+    	Position p4 = new Position(0,0);Position p5 = new Position(0,0);Position p6 = new Position(0,0);
+    	Position p7 = new Position(0,0);Position p8 = new Position(0,0);
+    	
+    	if (this.getPosition().getCol()-1>=0 && this.getPosition().getRow()-2>=0)
+			p1 = new Position(this.getPosition().getRow()-2, this.getPosition().getCol()-1);
+		if (this.getPosition().getCol()+1<8 && this.getPosition().getRow()-2>=0)
+			p2 = new Position(this.getPosition().getRow()-2, this.getPosition().getCol()+1);
+		if (this.getPosition().getCol()-1>=0 && this.getPosition().getRow()+2<8)
+			p3 = new Position(this.getPosition().getRow()+2, this.getPosition().getCol()-1);
+		if (this.getPosition().getCol()+1<8 && this.getPosition().getRow()+2<8)
+		    p4 = new Position(this.getPosition().getRow()+2, this.getPosition().getCol()+1);
+		if (this.getPosition().getCol()+2<8 && this.getPosition().getRow()-1>=0)
+		    p5 = new Position(this.getPosition().getRow()-1, this.getPosition().getCol()+2);
+		if (this.getPosition().getCol()+2<8 && this.getPosition().getRow()+1<8)
+		    p6 = new Position(this.getPosition().getRow()+1, this.getPosition().getCol()+2);
+		if (this.getPosition().getCol()-2>=0 && this.getPosition().getRow()-1>=0)
+			p7 = new Position(this.getPosition().getRow()-1, this.getPosition().getCol()-2);
+		if (this.getPosition().getCol()-2>=0 && this.getPosition().getRow()+1<8)
+			p8 = new Position(this.getPosition().getRow()+1, this.getPosition().getCol()-2); 	
+		
+		Position[] potentials = { p1, p2, p3, p4, p5, p6, p7, p8 };			
+		
+		for (Position positionToLookAt : potentials){
+			BoardSpace spaceToLookAt = game.getBoardSpace(positionToLookAt);
+			if (spaceToLookAt == null){
+    			continue;
+    		}
+			if (spaceToLookAt.isEmpty()) {
+				legalMoves.add(positionToLookAt);
+			}
+			else if (spaceToLookAt.getPiece().getPieceColor() != this.getPieceColor()) {
+				legalMoves.add(positionToLookAt);
+			}
+			else {
+    			continue;
+    		}
+		}
+		
+		if (removeMovesThatCauseCheck)
+    		removeMovesThatCauseCheck(game, legalMoves);
+    	
+        return legalMoves;
     }
 
     @Override
